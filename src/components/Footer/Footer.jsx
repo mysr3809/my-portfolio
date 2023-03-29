7a8b051684caa486c40561fce1bd0ba45ed92ada
mysr3809 <myasar0938@gmail.com>
2023-03-29 18:26:37 +0200

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,44 @@
 import React from 'react';
 import './footer.css';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub } from 'react-icons/ai';
+
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        MY
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+          <AiFillLinkedin />
+        </a>
+        <a href="https://github.com" target="_blank" rel="noreferrer">
+          <AiFillGithub />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Mustafa Yasar. All Rights Reserved</small>
+      </div>
+    </footer>
+  );
 };
 export default Footer;
